6d3084b27c53bca3fd3a43a2fe2dafbb2b6cd09f
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2025-03-10 16:06:23 +0600

    icon

---
+++ React_icon.jsx
@@ -0,0 +1,12 @@
+import { FaCartArrowDown } from "react-icons/fa";
+
+
+const App = () => {
+
+    return <section>
+        <h2>Hello there!</h2>
+        <FaCartArrowDown />
+    </section>
+}
+
+export default App;
