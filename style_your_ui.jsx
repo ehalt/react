2cd44b39218bd28dba7c8e4198771f60c4c6de76
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2025-03-10 16:02:02 +0600

    ui style

---
+++ style_your_ui.jsx
@@ -0,0 +1,15 @@
+
+const App = () => {
+    const styles = {
+        color: 'white',
+        backgroundColor: 'crimson',
+        padding: '2rem'
+    };
+
+    return <section>
+        <h2>Hello there!</h2>
+        <h1 style={styles}>Styling this heading</h1>
+    </section>
+}
+
+export default App;
